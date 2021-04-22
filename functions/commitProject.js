ec19ceea05b88cb4d76238d027c4f383133c5406
Eliseu Videira <eliseuvideira1@gmail.com>
2021-04-22 11:43:57 -0300

    :sparkles: done so far

--- functions/commitProject.js
+++ functions/commitProject.js
@@ -1,9 +1,11 @@
 const { execSync } = require("child_process");
+const { withChDir } = require("./withChDir");
 
-exports.commitProject = () => {
-  execSync("git add -A", { stdio: "inherit" });
-  execSync("git commit -m ':tada: first commit'", { stdio: "inherit" });
-  execSync("yarn version --minor", { stdio: "inherit" });
-  execSync("git push -u origin master");
-  execSync("git push --tags");
-};
+exports.commitProject = dir =>
+  withChDir(dir, () => {
+    execSync("git add -A", { stdio: "inherit" });
+    execSync("git commit -m ':tada: first commit'", { stdio: "inherit" });
+    execSync("yarn version --minor", { stdio: "inherit" });
+    execSync("git push -u origin master");
+    execSync("git push --tags");
+  });
--- index.js
+++ index.js
@@ -14,6 +14,23 @@ const rimraf = require("rimraf");
 const { directory, name, repo, keywords } = yargs();
 
 withPrettyError(() => {
+  if (!/^[-@a-z0-9]+$/.test(name)) {
+    throw new Error(`invalid project name "${name}"`);
+  }
+  if (
+    !/^git@[\w\.]+:[\w\.@\:\/\-~]+\.git$|^https:\/\/[\w\.@\:\/\-~]+$/.test(repo)
+  ) {
+    throw new Error(`invalid repo "${name}"`);
+  }
+  const invalidKeywords = (keywords || []).map(
+    keyword => !/^[-a-z0-9]+$/.test(keyword)
+  );
+  if (invalidKeywords.length > 0) {
+    throw new Error(
+      `invalid keywords ${invalidKeywords.map(x => `"${x}"`).join(", ")}`
+    );
+  }
+
   const TEMPLATE = path.resolve(path.join(__dirname, "template"));
   const DESTINATION = path.resolve(path.join(process.cwd(), directory));
 
@@ -32,7 +49,7 @@ withPrettyError(() => {
 
     commitProject(DESTINATION);
   } catch (err) {
-    rimraf.sync(DESTINATION);
+    // rimraf.sync(DESTINATION);
     throw err;
   }
 });
